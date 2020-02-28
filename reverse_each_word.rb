8cb980442b2e9104a4873ad7670ef0abf567f992
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-28 17:19:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence1)
   original_array = sentence1.split(" ")
   reversed_array = []
   original_array.each do |sentence1|
-    reversed_array << sentence1.reversed
-    
+    reversed_array << sentence1.reverse
+  end
+  reversed_array
 end
