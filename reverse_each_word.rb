80c270dd9873eeaee5c16cf97f78ec74a43a0d6d
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-28 17:23:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(sentence2)
   array = sentence2.split(" ") 
   test_array = []
-  array.collect do|word| 
+  array.collect do|sentence2| 
     test_array << sentence2.reverse 
   end
   test_array.join(" ")
